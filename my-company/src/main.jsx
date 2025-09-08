65b42f0a2018dc20f5d94b51a235d6804c888e51
Samuel Akongbota <122745519+Reysam2@users.noreply.github.com>
2025-09-08 19:50:22 +0000

    add: build a simple company website with React

---
+++ my-company/src/main.jsx
@@ -0,0 +1,30 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Services from "./pages/Services.jsx";
+import Contact from "./pages/Contact.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Home,
+  },
+  {
+    path: "/about",
+    Component: About,
+  },
+  {
+    path: "/services",
+    Component: Services,
+  },
+  {
+    path: "/contact",
+    Component: Contact,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
---
+++ my-company/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../assets/components/Navbar";
+function Home() {
+  return (
+    <>
+      <Navbar />
+
+      <div style={{ padding: "20px" }}>
+        <h1>Welcome to Our Company</h1>
+        <p>We are dedicated to delivering excellence in all our services.</p>
+      </div>
+    </>
+  );
+}
+
+export default Home;
