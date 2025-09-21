3d3edcefaab288dea685eb89eed62ce424288341
Samuel Akongbota <122745519+Reysam2@users.noreply.github.com>
2025-09-21 21:39:32 +0000

    docs: change file responsible for page routing

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,24 @@
-// import RecipeList from "./components/RecipeList";
-// import AddRecipeForm from "./components/AddRecipeForm";
-import Router from './router/Router.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
+import EditRecipeForm from "./components/EditRecipeForm";
 
 function App() {
   return (
-    <>
-      {/* <AddRecipeForm />
-      <RecipeList /> */}
-      <Router />
+    <> 
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/create-recipes" element={<AddRecipeForm />} />
+
+          <Route path="/recipe-details/:recipeId" element={<RecipeDetails />} />
+          <Route path="/edit-recipe/:recipeId" element={<EditRecipeForm />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
