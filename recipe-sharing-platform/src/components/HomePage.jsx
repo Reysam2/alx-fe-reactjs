1db61802eb4abf31499275f298a6158e87db9880
Samuel Akongbota <122745519+Reysam2@users.noreply.github.com>
2025-10-08 00:47:29 +0000

    feat: set up React app

---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return ( 
+    <div>
+      <h1 className="text-blue-500">This is the home page of the recipe sharing platform</h1>
+    </div>
+   );
+}
+
+export default HomePage;
