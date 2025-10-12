b4cc6b6bb866c3c2b94cbb5faebffac03e152d7f
Samuel Akongbota <122745519+Reysam2@users.noreply.github.com>
2025-10-12 18:51:11 +0000

    feat: implement PostsComponent with React Query
    - Fetch posts from JSONPlaceholder using useQuery.
    - Handle loading, error, and success states.
    - Display posts in a styled grid layout.
    - Add "Refresh" button to manually refetch data.
    - Utilize React Query caching for improved performance.

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,43 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+function PostsComponent() {
+  const { data, isLoading, isError, error, refetch } = useQuery({
+    queryKey: ["Posts"],
+    queryFn: async () => {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+
+      console.log(res.data);
+      return res.data;
+    },
+  });
+
+  const handleRefetch = () => {
+     refetch()
+  };
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Something went wrong: {error.message || "Unknown error"}</p>;
+
+  return (
+    <div className="w-[100%] h-[100%] bg-gray-300 grid grid-cols-2 place-items-center gap-x-2 gap-y-7 p-2">
+      <div>
+        <button onClick={handleRefetch} className="border p-[0.5rem_1.6rem] hover:text-amber-200 hover:bg-amber-950 transition-all duration-200 ease-in-out active:scale-90">
+          Refresh
+        </button>
+      </div>
+      {data &&
+        data.map((post) => (
+          <div
+            className="w-70 h-70 border flex flex-col items-center justify-around p-4 hover:shadow-2xl hover:drop-shadow-2xl"
+            key={post.id}
+          >
+            <h1 className="text-amber-950">{post.title}</h1>
+            <p className="text-amber-600">{post.body}</p>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default PostsComponent;
